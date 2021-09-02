e51cc62de14f706fd4a2a837002370fd16efbb32
Mads Borregaard Edske Rasmussen <mads7473@zbc.dk>
2021-09-02 12:52:19 +0200

    Add project files.

---
+++ Automaten OOP/Automat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten_OOP
+{
+    class Automat
+    {
+
+        public Automat(string name, int price, int lagerstatus)
+        {
+            Name = name;
+            Price = price;
+            LagerStatus = lagerstatus;
+        }
+
+        public string Name { get; set; }
+
+        public int LagerStatus { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Automaten OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("===============================================================");
+                Console.WriteLine("===                         Automat                         ===");
+                Console.WriteLine("===============================================================");
+                Console.WriteLine("=== 1. Coca Cola      = 2. Fanta Orange    = 3. Faxe Kondi  ===\n\n=== 4. Haribo Matador = 5. Haribo ClickMix = 6. Skumbananer ===\n\n=== 7. Twix           = 8. Snickers        = 9. Mars        ===\n \n                    === 0. Administration ===\n");
+                Logic.Menu();
+                Console.Clear();
+            }
+        }
+    }
+}
